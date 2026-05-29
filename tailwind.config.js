ba2374fcf7e7c7e60bf8f928141f419e5bc71d84
sidharthsehgal <sidharth.sehgal@gmail.com>
2026-05-28 20:43:02 -0700

    Updated site theme

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html", "./BizCenterWaitlist.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Lora", "serif"]
+      },
+      colors: {
+        brand: {
+          navy: "#102941",
+          green: "#2F6F4E",
+          cream: "#F7F1E4",
+          linen: "#EFE2CB",
+          ink: "#11100E"
+        }
+      }
+    }
+  }
+};
